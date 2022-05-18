766c36dbd2d5c3a12a81cbae6d8ae747afbf4735
jakub <jk438520@students.mimuw.edu.pl>
2022-05-18 14:44:18 +0200

    init

--- src/main/java/pl/edu/mimuw/Main.java
+++ src/main/java/pl/edu/mimuw/Main.java
@@ -1,7 +1,24 @@
 package pl.edu.mimuw;
 
+import pl.edu.mimuw.resources.*;
+
 public class Main {
 
   public static void main(String[] args) {
+    try{
+      RandomExceptionUtils.justThrow();
+    } catch (ExceptionA | ExceptionB e) {
+      System.out.println("a or b");
+      System.out.println(e);
+    } catch (ExceptionC e) {
+      System.out.println("c");
+      System.out.println(e);
+    } catch (AbstractException e) {
+      System.out.println("abstract");
+      System.out.println(e);
+    } catch (ExceptionD e) {
+      System.out.println("d");
+      System.out.println(e);
+    }
   }
 }
---
+++ src/main/java/pl/edu/mimuw/resources/RandomExceptionUtils.java
@@ -0,0 +1,21 @@
+package pl.edu.mimuw.resources;
+
+import java.util.Random;
+
+public class RandomExceptionUtils {
+
+  private static Random RANDOM = new Random();
+
+  public static void justThrow() throws AbstractException{
+    switch (RANDOM.nextInt(4)){
+      case 0:
+        throw new ExceptionA("A");
+      case 1:
+        throw new ExceptionB("B");
+      case 2:
+        throw new ExceptionC("C");
+      case 3:
+        throw new ExceptionD("D");
+    }
+  }
+}
